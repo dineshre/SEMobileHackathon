8c9c395fe29fe98c2e8259f1ad2d5848a2d8df2e
dineshre <reddy1607>
2014-05-29 10:09:08 -0400

    Added bubble chart and JQuery UI

--- platforms/ios/www/app.js
+++ platforms/ios/www/app.js
@@ -5,14 +5,31 @@ var App = angular.module('helloKinvey', [ 'ngRoute', 'deviceModule', 'kinveyModu
 
 function loadTheApp() {
 
-    // Initiate FastClick
+    // Initiate FastClick - removes the touch screen click delay on mobile web pages
     FastClick.attach(document.body);
+
+    //prevent scrolling on body so app doesnt slide off of screen
+    //  document.body.addEventListener('touchmove', function(e) { e.preventDefault(); }, false);
 
     // Boot AngularJS
     try {
-        angular.bootstrap($('body'), ['helloKinvey']);
+        angular.bootstrap($('body'), ['KitchenSink']);
     } catch (e) {
         console.log("Angular didn't load " + e);
+    }
+
+    setTimeout(function () {
+        // Hide splash screen if any
+        if (navigator && navigator.splashscreen) {
+            navigator.splashscreen.hide();
+        }
+    },1000);
+
+
+    if (device.platform === 'android' || device.platform === 'Android') {
+        document.addEventListener("backbutton", function (e) {
+            e.preventDefault();
+        }, false);
     }
 
 
@@ -28,14 +45,62 @@ angular.element(document).ready(function() {
 });
 
 App.config(function ($routeProvider){
-	$routeProvider
+    $routeProvider
         .when('/', {
-            templateUrl:'views/page1.html',
-            controller: 'page1Controller'
+            templateUrl:'views/headers.html'
         })
-	.otherwise({
-		redirectTo:'/'
-	})
+        .when('/lists', {
+            templateUrl:'views/lists.html'
+        })
+        .when('/cards', {
+            templateUrl:'views/cards.html'
+        })
+        .when('/tabs', {
+            templateUrl:'views/tabs.html',
+            controller: 'tabsController'
+        })
+        .when('/dialog', {
+            templateUrl:'views/dialog.html',
+            controller: 'dialogController'
+        })
+        .when('/popin', {
+            templateUrl:'index.html',
+            controller: 'popinController'
+        })
+        .when('/forms', {
+            templateUrl:'views/forms.html',
+            controller: 'formsController'
+        })
+        .otherwise({
+            redirectTo:'/'
+        })
+
+});
+
+
+App.run(function($rootScope, $window) {
+
+    $rootScope.showLogin = true;
+    $rootScope.showPopin = false;
+    $rootScope.showDialog = false;
+    $rootScope.showAbout = false;
+    $rootScope.showForgotPassword = false;
+    $rootScope.showChangePassword = false;
+    $rootScope.slide = '';
+
+    $rootScope.$on('$routeChangeStart', function() {
+
+        //event button to move backward
+        $rootScope.back = function() {
+            $rootScope.slide = 'slide-right';
+            $window.history.back();
+
+        };
+        //event button item list to move forward
+        $rootScope.next = function() {
+            $rootScope.slide = 'slide-left';
+        }
+    });
 
 });
 
--- platforms/ios/www/controllers.js
+++ platforms/ios/www/controllers.js
@@ -1,27 +1,174 @@
-/* ********** page1Controller *************** */
-App.controller('page1Controller', function($scope, deviceInfo, kinveyServices, APPCONFIG) {
+/* ********** MainCtrl *************** */
+App.controller('MainCtrl', function($scope, $location, $rootScope) {
 
-    $scope.myItems = deviceInfo.myinfo().itemlisting;
+    //options for left drawer menu.  keep right menu disabled, sdk has not styled it yet.
+    $scope.snapOpts = {
+        disable: 'right',touchToDrag:false
+    };
 
-    var kinveyInit = kinveyServices.init(APPCONFIG.APIENDPOINT,APPCONFIG.APPKEY,APPCONFIG.APPSECRET);
-    kinveyInit.then(
-        function (success) {
-            $scope.myKinveyInit = success;
+    $rootScope.login = function(){
+        $rootScope.showLogin = false;
+        $scope.snapOpts = {
+            touchToDrag:true
+        };
+    }
 
-            var ping = kinveyServices.ping();
-            ping.then(
-                function (success2) {
-                    $scope.myKinveyPing = success2;
+    $rootScope.showAboutScreen = function(){
+        $rootScope.showAbout = true;
+    }
 
-                },
-                function (error2) {
-                    $scope.myKinveyPing = error2;
-                });
+    $rootScope.closeAboutScreen = function(){
+        $rootScope.showAbout = false;
+    }
 
-        },
-        function (error) {
-            $scope.myKinveyInit = error;
-        });
+    $rootScope.showForgotPass = function(){
+        $rootScope.showForgotPassword = true;
+    }
+
+    $rootScope.closeForgotPassword = function(){
+        $rootScope.showForgotPassword = false;
+    }
+
+    $rootScope.showChangePass = function(){
+        $rootScope.showChangePassword = true;
+    }
+
+    $rootScope.closeChangePassword = function(){
+        $rootScope.showChangePassword = false;
+    }
+
+    $scope.$on('$routeChangeSuccess', update);
+    function update(){
+        $rootScope.showPopin = false;
+        $rootScope.showDialog = false;
+    }
 
 });
 
+
+/* *********** NavigationCtrl ************** */
+App.controller('NavigationCtrl', ['$scope', '$location', function ($scope, $location) {
+
+    //function used inside senav directive.  checks for the current url to set an ACTIVE class on menu item
+    $scope.isCurrentPath = function (path) {
+        return $location.path() == path;
+    };
+
+
+    $scope.menuItems = {
+        "itemlisting":[
+            {"url": "/headers", "hasicon": true, "icon": "se-icon se-icon-information", "title": "Headers"},
+            {"url": "/lists", "hasicon": true, "icon": "se-icon se-icon-information", "title": "Lists"},
+            {"url": "/cards", "hasicon": true, "icon": "se-icon se-icon-information", "title": "Content Cards"},
+            {"url": "/tabs", "hasicon": true, "icon": "se-icon se-icon-information", "title": "Tabs"},
+            {"url": "/dialog", "hasicon": true, "icon": "se-icon se-icon-information", "title": "Dialog"},
+            {"url": "/popin", "hasicon": true, "icon": "se-icon se-icon-information", "title": "Popin"},
+            {"url": "/forms", "hasicon": true, "icon": "se-icon se-icon-information", "title": "Forms"}
+        ],
+        "containers":[
+            {"header": true, "search": true, "footer": true}
+        ]
+    };
+
+}]);
+
+
+/* *********** SecondaryNavigationCtrl ************** */
+App.controller('SecondaryNavigationCtrl', ['$scope', '$location', function ($scope, $location) {
+
+    $scope.pageTitle = '';
+    $scope.showBack = false;
+
+    $scope.$on('$routeChangeSuccess', update);
+    function update() {
+        switch ($location.path()) {
+            case '/':
+                $scope.pageTitle = 'HEADERS';
+                $scope.showBack = false;
+                break;
+            case '/lists':
+                $scope.pageTitle = 'Lists';
+                $scope.showBack = true;
+                break;
+            case '/cards':
+                $scope.pageTitle = 'CONTENT CARDS';
+                $scope.showBack = true;
+                break;
+            case '/tabs':
+                $scope.pageTitle = 'TABS';
+                $scope.showBack = true;
+                break;
+            case '/dialog':
+                $scope.pageTitle = 'DIALOG';
+                $scope.showBack = true;
+                break;
+            case '/popin':
+                $scope.pageTitle = 'POPIN';
+                $scope.showBack = true;
+                break;
+            case '/forms':
+                $scope.pageTitle = 'FORMS';
+                $scope.showBack = true;
+                break;
+            default:
+                $scope.pageTitle = '';
+                $scope.showBack = false;
+        }
+    }
+
+}]);
+
+
+
+/* ********** tabsController *************** */
+App.controller('tabsController', function($scope) {
+    $scope.tabs = 'tab4';
+
+    $scope.switchTabs = function(tab){
+        $scope.tabs = tab;
+    }
+
+    $scope.isActiveTab = function (tab) {
+        return $scope.tabs == tab;
+    };
+});
+
+/* ********** dialogController *************** */
+App.controller('dialogController', function($rootScope) {
+    $rootScope.showDialog = false;
+
+
+    $rootScope.dialogToggle = function(){
+        $rootScope.showDialog = (($rootScope.showDialog!=false) ? false : true);
+
+    }
+});
+
+/* ********** popinController *************** */
+App.controller('popinController', function($rootScope) {
+    $rootScope.showPopin = false;
+
+    $rootScope.popinToggle = function(){
+        $rootScope.showPopin = (($rootScope.showPopin!=false) ? false : true);
+
+    }
+});
+
+/* ********** formsController *************** */
+App.controller('formsController', function($scope) {
+
+    $scope.cb1 = false;
+    $scope.cb2 = true;
+
+    $scope.setButton = function(cb) {
+        if(cb == 'cb1'){
+            $scope.cb1 = (($scope.cb1!=false) ? false : true);
+        }
+
+        if(cb == 'cb2'){
+            $scope.cb2 = (($scope.cb2!=false) ? false : true);
+        }
+
+    }
+
+});
